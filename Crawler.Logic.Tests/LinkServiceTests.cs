9625c06c6c74fbfe2cc6ed977667625c075fbd15
ArtemSydora <artiom.sidora@gmail.com>
2021-08-26 19:18:09 +0300

    Write tests

--- Crawler.Logic.Tests/LinkServiceTests.cs
+++ Crawler.Logic.Tests/LinkServiceTests.cs
@@ -1,36 +1,122 @@
-﻿using Crawler.Entities;
-using Moq;
-using System.Data;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using Crawler.Entities;
+using Crawler.Repository;
 
 namespace Crawler.Logic.Tests
 {
     public class LinkServiceTests
     {
-        private readonly Mock<IRepository<Test>> _mockRepository;
+        private readonly Mock<RepositoryDataAccess> _mockRepositoryDataAccess;
         private readonly LinkService _linkService;
 
         public LinkServiceTests()
         {
-            _mockRepository = new Mock<IRepository<Test>>();
-            _linkService = new LinkService(_mockRepository.Object);
+            _mockRepositoryDataAccess = new Mock<RepositoryDataAccess>(null);
+            _linkService = new LinkService(_mockRepositoryDataAccess.Object);
         }
 
         [Fact]
-        public async Task SaveMeasuredLinks_ShouldAddTestToDatabase()
+        public async Task AddTestResultsAsync_ShouldPassResultsToDatabase()
         {
             //arrange
             var fakeHomePageUrl = "http://www.example.com";
             var fakeLinks = new[] { new Link() };
             var fakePings = new[] { new Ping() };
 
+            _mockRepositoryDataAccess
+               .Setup(rda => rda.SaveTestResultAsync(It.IsAny<string>(), It.IsAny<IEnumerable<MeasuredLink>>()));
+
             //act
-            await _linkService.SaveMeasuredLinks(fakeHomePageUrl, fakeLinks, fakePings);
+            await _linkService.AddTestResultsAsync(fakeHomePageUrl, fakeLinks, fakePings);
 
             //assert
-            _mockRepository.Verify(r => r.AddAsync(It.IsAny<Test>(), default), Times.Once);
-            _mockRepository.Verify(r => r.SaveChangesAsync(default), Times.Once);
+            _mockRepositoryDataAccess
+                .Verify(rda => rda.SaveTestResultAsync(It.IsAny<string>(), It.IsAny<IEnumerable<MeasuredLink>>()), Times.Once);
         }
+
+        [Fact]
+        public void GetPingsByUrlOrderByPing_ReturnOrderedPingsCollection()
+        {
+            //arrange
+            var fakeMeaseredLinkCollection = GetFakeMeasuredLinks();
+
+            _mockRepositoryDataAccess
+                .Setup(rda => rda.GetTestsByHomePageUrl(It.IsAny<string>()))
+                .Returns(new Test
+                {
+                    MeasuredLinks = fakeMeaseredLinkCollection
+                });
+
+            //act
+            IEnumerable<Ping> actual = _linkService.GetPingsByUrlOrderByPing(String.Empty);
+
+            //assert
+            Assert.Collection(actual,
+                ping => Assert.Equal(new Ping { Url = "1", ResponseTimeMs = 100 }, ping),
+                ping => Assert.Equal(new Ping { Url = "2", ResponseTimeMs = 200 }, ping),
+                ping => Assert.Equal(new Ping { Url = "3", ResponseTimeMs = 300 }, ping));
+        }
+
+        [Fact]
+        public void GetUniqueWebsiteLinksByUrl_ReturnLinksCollection()
+        {
+            //arrange
+            var fakeMeaseredLinkCollection = GetFakeMeasuredLinks();
+
+            _mockRepositoryDataAccess
+                .Setup(rda => rda.GetTestsByHomePageUrl(It.IsAny<string>()))
+                .Returns(new Test
+                {
+                    MeasuredLinks = fakeMeaseredLinkCollection
+                });
+
+            //act
+            IEnumerable<Link> actual = _linkService.GetUniqueWebsiteLinksByUrl(String.Empty);
+
+            //assert
+            Assert.Collection(actual,
+                link => Assert.Equal(new Link { Url = "2", InSitemap = false, InWebsite = true }, link));
+        }
+
+        [Fact]
+        public void GetUniqueSitemapLinksByUrl_ReturnLinksCollection()
+        {
+            //arrange
+            var fakeMeaseredLinkCollection = GetFakeMeasuredLinks();
+
+            _mockRepositoryDataAccess
+                .Setup(rda => rda.GetTestsByHomePageUrl(It.IsAny<string>()))
+                .Returns(new Test
+                {
+                    MeasuredLinks = fakeMeaseredLinkCollection
+                });
+
+            //act
+            IEnumerable<Link> actual = _linkService.GetUniqueSitemapLinksByUrl(String.Empty);
+
+            //assert
+            Assert.Collection(actual,
+                link => Assert.Equal(new Link { Url = "1", InSitemap = true, InWebsite = false }, link));
+        }
+
+        #region FakeData
+
+        private List<MeasuredLink> GetFakeMeasuredLinks()
+        {
+            var fakeMeaseredLinkCollection = new List<MeasuredLink>
+           {
+                new MeasuredLink { Url = "2", ResponseTimeMs = 200, InSitemap = false, InWebsite = true },
+                new MeasuredLink { Url = "3", ResponseTimeMs = 300, InSitemap = true, InWebsite = true },
+                new MeasuredLink { Url = "1", ResponseTimeMs = 100, InSitemap = true, InWebsite = false }
+           };
+
+            return fakeMeaseredLinkCollection;
+        }
+
+        #endregion
     }
 }
