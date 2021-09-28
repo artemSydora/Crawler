0b315a7c66853bc722493d97e2dc4e4f31a1a7d6
Artem Sydora <artiom.sidora@gmail.com>
2021-09-28 19:01:00 +0300

    Add and import new view models

---
+++ Crawler.Web/Models/DetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Crawler.Web.Models
+{
+    public class DetailViewModel
+    {
+        public string Url { get; set; }
+
+        public bool InSitemap { get; set; }
+
+        public bool InWebsite { get; set; }
+
+        public int ResponseTimeMs { get; set; }
+    }
+}
---
+++ Crawler.Web/Models/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Crawler.Web.Models
+{
+    public class TestViewModel
+    {
+        public string StartPageUrl { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
