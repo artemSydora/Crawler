1c8189ae21b9fcb72306dfe501f0ef7f4754a0c0
Artem Sydora <artem.sydora@ukad-group.com>
2021-10-04 10:13:38 +0300

    Add mapper

---
+++ Crawler.Api/Services/Mapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Crawler.Entities.Models;
+using Crawler.Service.Models;
+using Crawler.Api.Models;
+
+namespace Crawler.Api.Services
+{
+    public class Mapper
+    {
+        public IEnumerable<DetailViewModel> MapDetailViewModels(IEnumerable<DetailDTO> details)
+        {
+            return details
+                .Select(detail => new DetailViewModel
+                {
+                    InSitemap = detail.InSitemap,
+                    InWebsite = detail.InWebsite,
+                    ResponseTimeMs = detail.ResponseTimeMs,
+                    Url = detail.Url
+                });
+        }
+
+        public IEnumerable<TestViewModel> MapTestViewModels(IEnumerable<TestDTO> tests)
+        {
+            return tests
+                .Select(test => new TestViewModel
+                {
+                    DateTime = test.DateTime,
+                    StartPageUrl = test.StartPageUrl
+                });
+        }
+
+        public PageViewModel MapPageViewModel(PageModel page)
+        {
+            return new PageViewModel
+            {
+                CurrentPage = page.CurrentPage,
+                Tests = MapTestViewModels(page.Tests).ToList(),
+                TotalPages = page.TotalPages
+            };
+        }
+    }
+}
