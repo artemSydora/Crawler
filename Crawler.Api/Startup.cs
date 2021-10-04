33ba4dbf1f043248e140c39721dc61285ebe03a9
Artem Sydora <artem.sydora@ukad-group.com>
2021-10-04 17:30:06 +0300

    Add fault handler

--- Crawler.Api/Startup.cs
+++ Crawler.Api/Startup.cs
@@ -4,6 +4,7 @@ using Crawler.Repository.Extensions;
 using Crawler.Service.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,6 +41,17 @@ namespace Crawler.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later");
+                    });
+                });
             }
 
             app.UseHttpsRedirection();
