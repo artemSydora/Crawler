6abfc2a400081943696d06abec4a8ef6eff2d1ac
Artem Sydora <artem.sydora@ukad-group.com>
2021-10-04 10:13:26 +0300

    Add models

---
+++ Crawler.Api/Models/PageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Crawler.Api.Models
+{
+    public class PageViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<TestViewModel> Tests { get; set; }
+    }
+}
